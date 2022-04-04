9e38baebaa3084c376b96512538e8c9553bc28dc
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-04 20:00:56 -0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import * as C from './GlobalStyled'
+import RoutesAdd from './routes'
 import './App.css';
 
 function App() {
   return (
-
+    <>
+          <C.GlobalStyled />
+          <C.Container>
+              <RoutesAdd />
+          </C.Container>
+      </>
     
   );
 }
