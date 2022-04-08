67c4d4fe8aceb9ec3871ba0061a15a419797bcf4
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-07 21:23:44 -0300

    create component footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styled';
+
+const Footer: React.FC = () => {
+  return (
+      <Container>
+          <h1>Hello Footer</h1>
+      </Container>
+      
+  );
+}
+
+export default Footer;
