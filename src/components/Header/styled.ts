49155efec31d1f1a3d9789e43f28c80c51eaa195
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-07 21:24:47 -0300

    create styled header

---
+++ src/components/Header/styled.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 128px;
+    border: 0;
+    border-bottom: 1px solid #116FA8;
+`
+export const Logo = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+export const LogoName = styled.div`
+    font-size: 2rem;
+    font-weight: bold;
+    color: #0077C0;
+    margin-left: 20px;
+`
+export const Ul = styled.ul`
+    list-style: none;
+    display: flex;
+    align-items: center;
+`
+export const Li = styled.li`
+    font-size: 1.2rem;
+    padding: 0 30px;
+    color: #116FA8;
+    cursor: pointer;
+`
+export const Button = styled.button`
+    background-color: #116FA8;
+    color: #FFFFFF;
+    border: 0;
+    padding: 10px 20px;
+    border-radius: 4px;
+    cursor: pointer;
+
+`
