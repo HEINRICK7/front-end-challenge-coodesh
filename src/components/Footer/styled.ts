007b327aa93485b48ed57a5b664539d287af8af6
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-07 21:24:13 -0300

    create styled footer

---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    border: 0;
+    border-top: 1px solid #000000;
+    background-color: #0077C0;
+    color: #FFFFFF;
+    height: 11.2vh;
+   
+`
