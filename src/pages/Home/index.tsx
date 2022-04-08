cac2014b7eb419b933af4cd1ec1c3abe52e6d3e4
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-07 21:25:16 -0300

    modified page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
 
-import { Container } from './styled';
+import * as C from './styled';
+import Image from '../../assets/image.svg'
+
+import { useNavigate } from 'react-router-dom';
+
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 const Home: React.FC = () => {
-  return <div />;
+
+  const history = useNavigate();
+  
+  return (
+      <>
+        <C.Container>
+            <Header/>
+            <C.Main>
+                <C.SectionLeft>
+                    <C.Description>
+                        <C.Title>ÁREA DE GESTÃO</C.Title>
+                            <C.P>Tenha acesso às informações dos pacientes, 
+                                filtre e obtenha dados específicos de todos pacientes cadastrados no nosso sistema.
+                            </C.P>
+                        <C.Button onClick={() => { history('/list_users')}}>Entrar</C.Button>
+                    </C.Description>
+                </C.SectionLeft>
+                <C.Sectionright>
+                    <img src={Image} alt="doctor" />
+                </C.Sectionright>
+            </C.Main>
+            <Footer/>
+        </C.Container>
+       
+        
+      </>
+  );
 }
 
 export default Home;
--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -1,5 +1,78 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 
+`
+export const Main = styled.div`
+    height: 80vh;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 50px;
+`
+export const SectionLeft = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+
+
+`
+export const Description = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-items: flex-start;
+
+    width: 80%;
+    height: 50%;
+`
+export const Sectionright = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+export const Title = styled.div`
+  
+font-style: normal;
+font-weight: 600;
+font-size: 69px;
+line-height: 125%;
+
+letter-spacing: -0.222222px;
+
+color: #116FA8;
+margin-bottom: 30px;
+
+`
+export const P = styled.div`
+   font-style: normal;
+font-weight: 400;
+font-size: 30px;
+line-height: 125%;
+
+letter-spacing: -0.222222px;
+
+color: #116FA8;
+margin-bottom: 30px;
+
+    
+`
+export const Button = styled.div`
+    background: #0077C0;
+    border-radius: 4px;
+    padding: 20px 50px;
+    cursor: pointer;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 2rem;
+    line-height: 18px;
+    /* or 63% */
+
+    text-align: center;
+
+    color: #FFFFFF;
 `
