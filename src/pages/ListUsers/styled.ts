22ef6240c28fe5c6f056f9305b47e9ecdb7cd68e
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-07 21:26:00 -0300

    create styled ListUsers

---
+++ src/pages/ListUsers/styled.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+export const Main = styled.div`
+    margin: 50px auto;
+    width: 80%;
+    height: 80vh;
+    padding: 0 50px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    overflow-y: scroll;
+`
+export const ContainerModal = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`
+export const Img = styled.div`
+    margin-top: -70px;
+    border-radius: 50%;
+    box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
+    
+`
