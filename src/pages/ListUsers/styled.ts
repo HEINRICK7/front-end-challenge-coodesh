ca3fb54030938d11ab75cefbae35ff883509ccc3
HEINRICK7 <heinrickcostta@gmail.com>
2022-04-09 09:08:50 -0300

    update styled

--- src/pages/ListUsers/styled.ts
+++ src/pages/ListUsers/styled.ts
@@ -84,12 +84,24 @@ export const Img = styled.div`
     border-radius: 50%;
     box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
     
+    @media only screen and (max-width: 800px) {
+      
+      margin-top:0
+    }
 `
 export const ContainerCards = styled.div`
     display: flex;
     flex-direction: row;
     width: 100%;
     height: 40vh;
+
+
+    @media only screen and (max-width: 800px) {
+      
+      display: flex;
+      flex-direction: column;
+      height:100%;
+    }
     
 `
 export const Card = styled.div`
